7e649960c656ee5b453084c990be9ac3492afcf1
Jakub Niedźwiedź <jakub.niedzwiedz@gmail.com>
2026-08-08 17:55:35 +0200

    Format dbus-update-activation-environment command for better readability

--- chezmoi/private_dot_config/hypr/modules/autostart.lua
+++ chezmoi/private_dot_config/hypr/modules/autostart.lua
@@ -12,7 +12,16 @@ local workspaces = require("modules.workspaces")
 hl.on("hyprland.start", function()
 	hl.env("XDG_MENU_PREFIX", "arch-")
 	uwsm.start_raw(
-		"dbus-update-activation-environment --systemd WAYLAND_DISPLAY DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP XDG_SESSION_TYPE XDG_MENU_PREFIX GDK_BACKEND QT_QPA_PLATFORM QT_QPA_PLATFORMTHEME QT_STYLE_OVERRIDE"
+		"dbus-update-activation-environment --systemd "
+			.. "WAYLAND_DISPLAY DISPLAY "
+			.. "XDG_CURRENT_DESKTOP "
+			.. "XDG_SESSION_DESKTOP "
+			.. "XDG_SESSION_TYPE "
+			.. "XDG_MENU_PREFIX "
+			.. "GDK_BACKEND "
+			.. "QT_QPA_PLATFORM "
+			.. "QT_QPA_PLATFORMTHEME "
+			.. "QT_STYLE_OVERRIDE"
 	)
 	workspaces.setup_workspaces()
 end)
