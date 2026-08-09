a4746739c546148102f0712115d12ab53d0d3b07
Jakub Niedźwiedź <jakub.niedzwiedz@gmail.com>
2026-08-09 21:36:27 +0200

    Add temporary background

--- chezmoi/private_dot_config/hypr/modules/appearance/background.lua
+++ chezmoi/private_dot_config/hypr/modules/appearance/background.lua
@@ -4,8 +4,7 @@
 
 hl.config({
 	misc = {
-		force_default_wallpaper = -1, -- Set to 0 or 1 to disable the anime mascot wallpapers
-		disable_hyprland_logo = false, -- If true disables the random hyprland logo / anime girl background. :(
-		--focus_on_activate = true, -- This is to fix the Obsidian Web Clipper not being able to create notes.
+		force_default_wallpaper = 0, -- Set to 0 or 1 to disable the anime mascot wallpapers
+		disable_hyprland_logo = true, -- If true disables the random hyprland logo / anime girl background. :(
 	},
 })
